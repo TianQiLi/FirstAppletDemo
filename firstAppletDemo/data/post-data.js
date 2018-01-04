b6752d3a28359849506225ed9b7d864c86e491d3
litianqi <871651575@qq.com>
2018-01-04 10:58:50 +0800

    post js

---
+++ firstAppletDemo/data/post-data.js
@@ -0,0 +1,40 @@
+var newsListArray = [
+  {
+    "newTitle": "最近基金大涨啊",
+    "newsContent": "从今年九月底开始进入基金大涨阶段，截止到双十一后涨幅出现回落, 你怎么                                 看待今年媒体吹捧的慢牛呢",
+    "newsCount": 20,
+    "newsDate": "2017-11-20"
+  },
+  {
+    "newTitle": "A股不停波动",
+    "newsContent": "不错哦，a股大涨了额，港深股也不错",
+    "newsCount": 20,
+    "newsDate": "2017-10-10",
+  }, {
+    "newTitle": "B股波动",
+    "newsContent": "b股大涨了额，港深股也不错",
+    "newsCount": 10,
+    "newsDate": "2017-09-10",
+
+
+  }, {
+    "newTitle": "B股波动",
+    "newsContent": "b股大涨了额，港深股也不错",
+    "newsCount": 10,
+    "newsDate": "2017-09-10",
+  }, {
+    "newTitle": "c股波动",
+    "newsContent": "b股大涨了额，港深股也不错",
+    "newsCount": 10,
+    "newsDate": "2017-09-10",
+  }, {
+    "newTitle": "c股波动",
+    "newsContent": "b股大涨了额，港深股也不错",
+    "newsCount": 10,
+    "newsDate": "2017-09-10",
+  }
+]
+
+module.exports ={
+  "postList": newsListArray
+}
